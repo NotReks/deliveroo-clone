490e3a239b6231bbc5df291ba8aa39af70670e9d
Fahad <fahadadsani@gmail.com>
2023-01-24 22:54:03 +0300

    test

---
+++ screens/components/CategoryCard.jsx
@@ -0,0 +1,22 @@
+import { Image, Text, TouchableOpacity,} from 'react-native'
+import React from 'react'
+
+const CategoryCard = ({imgUrl, title}) => {
+  return (
+    <TouchableOpacity className='relative mr-2'>
+        <Image 
+        source={{
+            uri: imgUrl,
+        }}
+        style={{height:75, width:75, borderRadius:10}}
+        />
+      <Text 
+      style={{position:'absolute',bottom:1,left:1,color:'white',fontWeight:'bold'}}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CategoryCard
